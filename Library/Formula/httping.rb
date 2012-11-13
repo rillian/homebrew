2ccc9c5105843391ae469f466b53c236199a792e
Ralph Giles <giles@mozilla.com>
2012-11-13 11:33:53 -0800

    Update httping to 1.5.5.
    The 1.5.3 source url from the previous commit is 404, so this
    is necessary for the software to build at all.

--- Library/Formula/httping.rb
+++ Library/Formula/httping.rb
@@ -2,13 +2,8 @@ require 'formula'
 
 class Httping < Formula
   homepage 'http://www.vanheusden.com/httping/'
-  url 'http://www.vanheusden.com/httping/httping-1.5.3.tgz'
-  sha1 '3606bb3b2d899c8f3fb178a295520e113bcb8e56'
-
-  def patches
-    # fixes conflicting definitions of strdup()
-    { :p0 => 'https://trac.macports.org/export/88419/trunk/dports/net/httping/files/patch-strndup.diff' }
-  end
+  url 'http://www.vanheusden.com/httping/httping-1.5.5.tgz'
+  sha1 '5317724ab757e3f1a9a4633936cedb6d57349796'
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
